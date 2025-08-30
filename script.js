570b0b8f744f3d0e7f589fc586f7920c36ae5433
jdy7149 <184757480+jdy7149@users.noreply.github.com>
2025-08-31 01:02:28 +0900

    End Book's constructor

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-const myLibrary = [];
+const myLibrary = [
+    new Book('To Kill a Mockingbird',
+        'Harper Lee',
+        281,
+        true
+    ),
+    new Book('1984',
+        'George Orwell',
+        328,
+        false
+    ),
+    new Book('The Great Gatsby',
+        'F. Scott Fitzgerald',
+        180,
+        true
+    )
+];
 
-function Book() {
-
+function Book(title, author, pages, isRead) {
+    if (!new.target) 
+        throw Error("Need to use 'new' operator to call the constructor");
+    
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
 }
 
 function addBookToLibrary() {
-    
+
 }
